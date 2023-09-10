e778358b83458a1ee477c61544e9621664f84f0f
Marco Gomiero <marco.gomiero.93@gmail.com>
2023-09-10 20:38:31 +0200

    Make class internal

--- shared/src/commonMain/kotlin/com/prof18/feedflow/presentation/model/FeedError.kt
+++ shared/src/commonMain/kotlin/com/prof18/feedflow/presentation/model/FeedError.kt
@@ -2,8 +2,8 @@ package com.prof18.feedflow.presentation.model
 
 sealed interface ErrorState
 
-data class FeedErrorState(
+internal data class FeedErrorState(
     val failingSourceName: String,
 ) : ErrorState
 
-object DatabaseError : ErrorState
+internal data object DatabaseError : ErrorState

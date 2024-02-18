7c56b45c4d2334cafd09f86df18a12ae3f68a924
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-02-18 19:14:20 +0100

    Add Slovak language code

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -124,6 +124,7 @@ val macExtraPlistKeys: String
           <string>pl</string>
           <string>nb-rNO</string>
           <string>de</string>
+          <string>sk</string>
         </array>
     """.trimIndent()
 

f1d737dcff8eb49aa9ff3e9c9aef332d45eaa9e2
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-04-23 23:53:53 +0200

    Reduce font on Android reader mode

--- androidApp/src/main/kotlin/com/prof18/feedflow/android/readermode/ReaderModeHtmlAndCss.kt
+++ androidApp/src/main/kotlin/com/prof18/feedflow/android/readermode/ReaderModeHtmlAndCss.kt
@@ -61,7 +61,7 @@ internal fun readerModeCss(colors: ReaderColors) =
       padding-right: 16px;
       color: ${colors.textColor};
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-      font-size: 18px;
+      font-size: 16px;
       line-height: 1.5em;
     }
     figure {

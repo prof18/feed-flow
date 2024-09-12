c8bc53e1c8d14f791e96386c4f1f8b553df29329
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-09-12 22:59:16 +0200

    Add script for fdroid release

---
+++ .scripts/fdroid-release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION_CODE=$(git rev-list HEAD --first-parent --count)
+VERSION_NAME=$(git describe --tags --abbrev=0  --match "*-android")
+
+echo "VERSION_CODE: $VERSION_CODE"
+echo "VERSION_NAME: $VERSION_NAME"
+
+VERSION_NAME=${VERSION_NAME%-android}
+
+git checkout -b "release-fdroid-$VERSION_NAME"
+
+echo "Replace versionCode and versionName in androidApp/build.gradle.kts"
+sed -i '' "s/versionCode = getVersionCode()/versionCode = $VERSION_CODE/" ../androidApp/build.gradle.kts
+sed -i '' "s/versionName = getVersionName()/versionName = \"$VERSION_NAME\"/" ../androidApp/build.gradle.kts
+
+git commit -a -m "Release for fdroid: $VERSION_NAME"
+git tag "$VERSION_NAME-fdroid"
+git push origin "$VERSION_NAME-fdroid"
+git checkout main
+git branch -d "$VERSION_NAME-fdroid"
+
--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.prof18.feedflow"
         minSdk = libs.versions.android.min.sdk.get().toInt()
         targetSdk = libs.versions.android.target.sdk.get().toInt()
-        versionCode = getVersionCode()
-        versionName = getVersionName()
+        versionCode = 796
+        versionName = "0.0.58"
 
         addManifestPlaceholders(
             mapOf(

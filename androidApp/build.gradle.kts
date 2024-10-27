6532755618db0659d2288027fd1d4d4aec92bc61
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-10-27 21:33:55 +0100

    Release for fdroid: 0.0.59

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.prof18.feedflow"
         minSdk = libs.versions.android.min.sdk.get().toInt()
         targetSdk = libs.versions.android.target.sdk.get().toInt()
-        versionCode = getVersionCode()
-        versionName = getVersionName()
+        versionCode = 855
+        versionName = "0.0.59"
 
         addManifestPlaceholders(
             mapOf(

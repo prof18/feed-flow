45f395bd3d539b97b2718d2c7bfcf06f18d5d65d
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-03-01 16:30:15 +0100

    Add feed from share sheet on iOS - Fix #440

---
+++ iosApp/ShareExtension/ShareViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by Marco Gomiero on 06/01/25.
+//  Copyright © 2025 FeedFlow. All rights reserved.
+//
+
+import Foundation
+import Social
+import SwiftUI
+import UIKit
+
+@objc(ShareViewController)
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        startKoin()
+        let contentView = UIHostingController(
+            rootView: ShareView(extensionContext: extensionContext)
+        )
+        addChild(contentView)
+        view.addSubview(contentView.view)
+
+        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+        contentView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        contentView.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            contentView.view.heightAnchor.constraint(equalToConstant: 450) // Increased from 300
+        ])
+
+        contentView.view.layer.cornerRadius = 16
+        contentView.view.clipsToBounds = true
+    }
+}

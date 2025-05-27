a27ff3b34e0f5c90be2352204f2e675417eccb17
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-05-27 20:48:38 +0200

    Fix unescaped chars on title

--- sharedUI/src/jvmMain/kotlin/com/prof18/feedflow/shared/ui/utils/ImageLoader.jvm.kt
+++ sharedUI/src/jvmMain/kotlin/com/prof18/feedflow/shared/ui/utils/ImageLoader.jvm.kt
@@ -4,10 +4,9 @@ import coil3.PlatformContext
 import coil3.disk.DiskCache
 import okio.FileSystem
 
-@Suppress("MagicNumber")
 internal actual fun newDiskCache(context: PlatformContext): DiskCache {
     return DiskCache.Builder()
         .directory(FileSystem.SYSTEM_TEMPORARY_DIRECTORY / "image_cache")
-        .maxSizeBytes(512L * 1024 * 1024) // 512MB
+        .maxSizeBytes(size = 512L * 1024 * 1024) // 512MB
         .build()
 }

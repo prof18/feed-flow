8af960c23e81b98eff5c401844ab09cdd9b3ce37
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-09-12 20:45:07 +0200

    Update changelogs

--- .scripts/last-tags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "ANDROID"
-git describe --tags --abbrev=0  --match "*-android"
-echo "Remember to update the release notes for fdroid"
-echo "\nIOS"
-git describe --tags --abbrev=0  --match "*-ios"
-echo "\nDESKTOP"
-git describe --tags --abbrev=0  --match "*-desktop"

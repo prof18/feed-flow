e57fa45ae888fe36ff283d2c3f622281dbb14898
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-09-15 14:47:11 +0200

    Add title on subject on share sheet

--- androidApp/src/main/kotlin/com/prof18/feedflow/android/Share.kt
+++ androidApp/src/main/kotlin/com/prof18/feedflow/android/Share.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 
 internal fun Context.openShareSheet(title: String?, url: String) {
+    val subject = if (!title.isNullOrBlank()) title else url
     val sendIntent = Intent().apply {
         action = Intent.ACTION_SEND
         putExtra(Intent.EXTRA_TEXT, url)
-        title?.let { putExtra(Intent.EXTRA_SUBJECT, it) }
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TITLE, subject)
         type = "text/plain"
     }
     val shareIntent = Intent.createChooser(sendIntent, null)

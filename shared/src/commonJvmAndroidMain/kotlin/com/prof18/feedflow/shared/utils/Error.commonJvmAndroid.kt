c71315f14d2a8ab4a5d0e1e9bd86628111082c3e
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-12-15 12:55:39 +0100

    Exclude SSL and connection exceptions from Sentry

--- shared/src/commonJvmAndroidMain/kotlin/com/prof18/feedflow/shared/utils/Error.commonJvmAndroid.kt
+++ shared/src/commonJvmAndroidMain/kotlin/com/prof18/feedflow/shared/utils/Error.commonJvmAndroid.kt
@@ -1,8 +1,16 @@
 package com.prof18.feedflow.shared.utils
 
 import okhttp3.internal.http2.StreamResetException
+import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import javax.net.ssl.SSLHandshakeException
+import javax.net.ssl.SSLException
 
 actual fun Throwable.ignoreError(): Boolean =
-    this is SocketTimeoutException || this is StreamResetException || this is UnknownHostException
+    this is SocketTimeoutException ||
+        this is StreamResetException ||
+        this is UnknownHostException ||
+        this is SSLHandshakeException ||
+        this is SSLException ||
+        this is ConnectException

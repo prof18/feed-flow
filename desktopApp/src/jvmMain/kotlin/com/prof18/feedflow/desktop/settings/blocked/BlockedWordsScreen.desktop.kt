921c19e9d169cfb3e4adfa46392530c073012f96
Marco Gomiero <marco.gomiero.93@gmail.com>
2026-02-17 23:24:54 +0100

    Move to Window for Blocked words menu on desktop

--- desktopApp/src/jvmMain/kotlin/com/prof18/feedflow/desktop/settings/blocked/BlockedWordsScreen.desktop.kt
+++ desktopApp/src/jvmMain/kotlin/com/prof18/feedflow/desktop/settings/blocked/BlockedWordsScreen.desktop.kt
@@ -2,21 +2,34 @@ package com.prof18.feedflow.desktop.settings.blocked
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import com.prof18.feedflow.desktop.ui.components.DesktopDialogWindow
 import com.prof18.feedflow.shared.presentation.BlockedWordsViewModel
-import com.prof18.feedflow.shared.ui.settings.BlockedWordsScreenContent
+import com.prof18.feedflow.shared.ui.settings.BlockedWordsContent
+import com.prof18.feedflow.shared.ui.utils.LocalFeedFlowStrings
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 internal fun BlockedWordsScreen(
-    navigateBack: () -> Unit,
+    visible: Boolean,
+    onCloseRequest: () -> Unit,
 ) {
     val viewModel = koinViewModel<BlockedWordsViewModel>()
     val keywords = viewModel.wordsState.collectAsState().value
+    val title = LocalFeedFlowStrings.current.settingsBlockedWords
 
-    BlockedWordsScreenContent(
-        keywords = keywords,
-        onBackClick = navigateBack,
-        onAddWord = viewModel::onAddWord,
-        onRemoveWord = viewModel::onRemoveWord,
-    )
+    DesktopDialogWindow(
+        title = title,
+        size = DpSize(500.dp, 700.dp),
+        visible = visible,
+        onCloseRequest = onCloseRequest,
+    ) { modifier ->
+        BlockedWordsContent(
+            keywords = keywords,
+            onAddWord = viewModel::onAddWord,
+            onRemoveWord = viewModel::onRemoveWord,
+            modifier = modifier,
+        )
+    }
 }
